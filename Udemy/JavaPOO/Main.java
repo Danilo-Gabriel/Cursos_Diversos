c634ec92044fc2f9f7d10d3d58f29e0ffb061b01
Danilo_Gabriel <danilonascimento.dr@gmail.com>
2023-07-04 15:05:56 -0300

    finalizandoDia

--- Udemy/JavaPOO/Main.java
+++ Udemy/JavaPOO/Main.java
@@ -1,8 +1,22 @@
-public class Main {
-
+import java.util.Scanner;
+public class Main{
     public static void main(String[] args) {
-        System.out.println("HEllo Word");
+
+        Scanner sc = new Scanner(System.in);
+    /* EX01
+     * int N1, N2, Soma;
+        System.out.println("Digite um numero: ");
+        N1 = sc.nextInt();
+        System.out.println("Digite o segundo numero: ");
+        N2 = sc.nextInt();
+
+        Soma = N1 + N2;
+
+        System.out.println("A soma dos valores são: "+Soma);
+     */
+        
+
+        
+
     }
-
-
 }
