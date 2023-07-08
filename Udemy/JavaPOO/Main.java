25371f63198eeaedc33562a56273140788ca20d8
daniloGabriel <danilonascimento.dr@gmail.com>
2023-07-08 16:02:00 -0300

    up

--- Udemy/JavaPOO/Main.java
+++ Udemy/JavaPOO/Main.java
@@ -1,43 +1,9 @@
-import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-    /* EX01
-        int N1, N2, Soma;
-        System.out.println("Digite um numero: ");
-        N1 = sc.nextInt();
-        System.out.println("Digite o segundo numero: ");
-        N2 = sc.nextInt();
-
-        Soma = N1 + N2;
-
-        System.out.println("A soma dos valores são: "+Soma);
-    */
-    /*
-        EX 02
-       double area, raio, pi = 3.14159;
-  
-
-        System.out.println("Digite o valor do Raio: ");
-        raio = sc.nextDouble();
-        area = pi * (raio * raio);
-      
-        System.out.printf("Valor da área deste circulo: %.2f%n ", area);
-     
-     */
-
-        
-
-             
-
-        
-
-
-
-
-
-        
+       
+       
 
         
 
